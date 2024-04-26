01ddc1892da9d8050e78bd9b7e55922ce2bffe2e
jaymin-codes <jayminmistry1008@gmail.com>
2024-04-26 10:22:44 +0530

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+
+//screens
 import HomeScreen from "./screens/HomeScreen.jsx";
+import ProductScreen from "./screens/ProductScreen.jsx";
+
+//react-router-dom
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -9,22 +14,28 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+//redux
+import { Provider } from "react-redux";
+import store from "./store.js";
+
+//styles
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
-import ProductScreen from "./screens/ProductScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}> 
-    {/* Outlet in <App /> will render the below children */}
+    <Route path="/" element={<App />}>
+      {/* Outlet in <App /> will render the below children */}
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
-  ) 
+  )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
